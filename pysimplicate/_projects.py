5b1ed8dbff8d45e870dea5c9910ba03c87d943c7
HP Harmsen <hph@oberon.nl>
2020-12-22 18:10:03 +0100

    publish  Tue Dec 22 18:10:03 CET 2020

--- pysimplicate/_projects.py
+++ pysimplicate/_projects.py
@@ -7,7 +7,7 @@ def projects(self, from_date: str = '', until_date: str = '', status: str = ''):
     if status:
         status_dict = self.project_status()
         assert status in status_dict.keys, "Status can only be one of {status_dict.keys}"
-        url = self.add_url_param(url, '&q[project_status.id]', status_dict[status] )
+        url = self.add_url_param(url, '&q[project_status.id]', status_dict[status])
     result = self.call(url)
     return result
 
@@ -17,10 +17,11 @@ def projectstatus(self):
     result = self.call(url)
     return result
 
+
 def project_status_options(self):
     # Retrieves the list of project statusses and caches it
-    if not hasattr(self,'_project_status_dict'):
-        self._project_status_dict = {status['label']:status['id'] for status in self.project_status()}
+    if not hasattr(self, '_project_status_dict'):
+        self._project_status_dict = {status['label']: status['id'] for status in self.project_status()}
     return self._project_status_dict
 
 
