b1dabad74c1466d5a1167f960ff6204bcd806328
HP Harmsen <hph@oberon.nl>
2021-01-29 22:53:04 +0100

    Added sales pipeline

--- pysimplicate/_sales.py
+++ pysimplicate/_sales.py
@@ -10,3 +10,28 @@ def revenuegroup_dict(self):
     if not hasattr(self, '_revenuegroup_dict'):
         self._revenuegroup_dict = {r['label']: r['id'] for r in self.revenuegroup()}
     return self._revenuegroup_dict
+
+def sales(self):
+    url = '/sales/sales?'
+    result = self.call( url )
+    return result
+
+def sales_flat(self):
+    return [{
+        'organization':s['organization']['name'],
+        'employee':double_get(s, 'responsible_employee', 'name'),
+        'progress_label':s['progress']['label'],
+        'progress_position':s['progress']['position'],
+        'status':s['status']['label'],
+        'start_date':s['start_date'],
+        'modified':s['modified'],
+        'source':double_get(s,'source','name'),
+        'expected_revenue':s['expected_revenue'],
+        'chance_to_score':s['chance_to_score'],
+        'value':s['expected_revenue']*s['chance_to_score']/100
+    } for s in self.sales() if s['status']['label']=='OpportunityStatus_open']
+
+def double_get( obj, first, second ):
+    if obj.get(first):
+        return obj[first].get( second, '')
+    return ''
