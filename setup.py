465998bdfa74eb6b873023f9c6f3cc862e51f799
HP Harmsen <hph@oberon.nl>
2022-08-25 17:14:56 +0200

    publish  Thu Aug 25 17:14:56 CEST 2022

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ setuptools.setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "License :: The Unlicense",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
